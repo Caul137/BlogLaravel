c3742ef073174f32661e3d8459f104e3570e3d2a
Caul137 <146390141+Caul137@users.noreply.github.com>
2025-06-11 08:48:14 -0300

    up2

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
             {{ __('Seus Posts') }}
         </h2>
     </x-slot>
@@ -12,54 +14,45 @@
     <a href="/">Pagina inicial</a>
 
     <hr>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <table class="w-full">
-                    <th>
-                        <tr>
-                            <th class="px-2 py-2 text-left">id</th>
-                            <th class="px-2 py-2 text-left">Titulo</th>
-                            <th class="px-2 py-2 text-left">Criado em</th>
-                            <th class="px-2 py-2 text-left">Ações</th>
-                            <th class="px-2 py-2 text-left">Imagem</th>
+    
+    <main class="blogs">
+
+ 
+                @forelse($postsAuth as $postA)
+
+                    <div class="blogContainerAdmin">
+
+                        <div class="blogTitle">
+                            <h3> {{ $postA['title'] }} </h3>
+                        </div>
+
+                        @if ($postA->thumb)
+                            <img class="blogImg" src="{{ asset('storage/' . $postA->thumb) }} ">
+                        @endif
+
+                        <blog class="Content">
+                            {{ $postA['content'] }}
+                        </blog>
+
+                        <div class="blogDate">
+                            {{ $postA['created_at'] }}
+                        </div>
+
+                        <div class="blogSlug">
+                            {{ $postA['slug'] }}
+                        </div>
+                         <form action="{{ route('delete', $postA->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <button class="delePostAdmin">Deletar</button>
+                    </form>
+                    </div>
+                   
+
+                @empty
+                     <span colspan="4">Nâo existe nenhum blog</>
+                @endforelse
 
 
-
-                        </tr>
-                    </th>
-
-
-                    <tbody>
-                        @forelse($postsAuth as $postA)
-                            <tr>
-                                <td class="px-2 py-2"> {{ $postA['id'] }} </td>
-                                <td class="px-2 py-2"> {{ $postA['title'] }} </td>
-                                <td class="px-2 py-2"> {{ $postA['created_at'] }} </td>
-                                <td class="px-2 py-2"> <a href="/admin/posts/edit/{{ $postA['id'] }}">Editar</a> </td>
-                                <td class="px-2 py-2">
-                                    <form action="{{ route('delete', $postA->id) }}" method="POST"
-                                        enctype="multipart/form-data">
-                                        @csrf
-                                        <button>Deletar</button>
-                                    </form>
-                                </td>
-                                @if ($postA->thumb)
-                                    <td class="px-2 py-2"> <img src="{{ asset('storage/' . $postA->thumb) }}"> </td>
-                                @endif
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="4">Nâo existe nenhum blog</td>
-                            </tr>
-
-                        @endforelse
-
-                    </tbody>
-
-                </table>
-            </div>
-        </div>
-    </div>
+   </main>
 
 </x-app-layout>
